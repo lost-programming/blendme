b440f97658f5676f6a2ebbd15bbf1c20ccc364b9
sanghyeok <pash9801@naver.com>
2023-11-10 14:34:29 +0900

    필요없는 부분 삭제

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCH1AsYz4HGxRzqusi_GJhq9c3DmhFza_s",
-	authDomain: "blendme-6e839.firebaseapp.com",
-	projectId: "blendme-6e839",
-	storageBucket: "blendme-6e839.appspot.com",
-	messagingSenderId: "662005109090",
-	appId: "1:662005109090:web:addbf987ff1d8d0616dc3b",
-	measurementId: "G-FWVG6GVJ87"
+  apiKey: "AIzaSyCH1AsYz4HGxRzqusi_GJhq9c3DmhFza_s",
+  authDomain: "blendme-6e839.firebaseapp.com",
+  projectId: "blendme-6e839",
+  storageBucket: "blendme-6e839.appspot.com",
+  messagingSenderId: "662005109090",
+  appId: "1:662005109090:web:addbf987ff1d8d0616dc3b",
+  measurementId: "G-FWVG6GVJ87",
 };
 
 export const app = initializeApp(firebaseConfig);
