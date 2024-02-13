e7239259cc67cda08c889355cd47ab8f3bf8942b
dnwnvus <dnwnvus@gmail.com>
2024-02-13 21:19:02 +0900

    이미지 경로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,7 +115,7 @@ const Home = () => {
           return (
             <RoastingCard
               bean={bean}
-              image={`blendme/${bean.image}`}
+              image={`./${bean.image}`}
               key={"beanImage" + index}
               clickEvent={() => routerEvent(bean.name_en)}
             />
