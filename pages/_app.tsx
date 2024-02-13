2156cd2c8ac2cfe7cb7e3cb86500961bed13807c
sanghyeok <pash9801@naver.com>
2024-02-13 16:05:34 +0900

    로컬환경 리다이렉팅 임시 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/app.scss";
 import Head from "next/head";
 import Header from "../components/layout/header";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname === "/_error") router.push("/");
+  }, []);
+
   return (
     <RecoilRoot>
       <Head>
