440d7763c979fe7bc3a28a2be1b5ba6b3390bf36
dnwnvus <dnwnvus@gmail.com>
2024-02-13 18:58:49 +0900

    main page 이미지 경로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -114,7 +114,7 @@ const Home = () => {
           return (
             <RoastingCard
               bean={bean}
-              image={`public/${bean.image}`}
+              image={`./${bean.image}`}
               key={"beanImage" + index}
               clickEvent={() => routerEvent(bean.name_en)}
             />
